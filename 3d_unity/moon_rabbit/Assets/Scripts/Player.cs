dbfaa8f984dbca5acf615aedcfd6aa1bbfe00ee4
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-10-24 18:29:17 +0900

    Player 초기 작성

---
+++ 3d_unity/moon_rabbit/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 MoveVector = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMove();
+    }
+
+    void UpdateMove()
+    {
+        
+    }
+
+    public void ProcessInput(Vector3 moveDirection)
+    {
+
+    }
+}
