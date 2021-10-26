0fbf09fb9eecb76e2efd8193a17e990ab983b160
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-10-27 01:32:38 +0900

    시스템 매니저 중간완성

---
+++ 3d_unity/moon_rabbit/Assets/Scripts/SystemManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemManager : MonoBehaviour
+{
+    static SystemManager intance = null;
+
+    public SystemManager Instance
+    {
+        get
+        {
+            return intance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (intance != null)
+        {
+            Debug.LogError("SystemManager error! Singletone error!");
+            Destroy(gameObject);
+            return;
+        }
+        intance = this;
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
