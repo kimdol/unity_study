7dcd294bca5eb01814a3d548bdc2ab01908681b7
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-11-01 23:33:35 +0900

    충돌 처리 초기단계

--- 3d_unity/moon_rabbit/Assets/Scripts/Bullet.cs
+++ 3d_unity/moon_rabbit/Assets/Scripts/Bullet.cs
@@ -9,6 +9,8 @@ public enum OwnerSide : int
 }
 public class Bullet : MonoBehaviour
 {
+    const float LifeTime = 15.0f;
+
     OwnerSide ownerSide = OwnerSide.Player;
 
     [SerializeField]
@@ -17,7 +19,10 @@ public class Bullet : MonoBehaviour
     [SerializeField]
     float Speed = 0.0f;
 
+    float FiredTime;
     bool NeedMove = false;
+
+    bool Hited = false;
 
 
     // Start is called before the first frame update
@@ -29,6 +34,9 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (ProcessDisappearCondition())
+            return;
+
         UpdateMove();
     }
 
@@ -37,8 +45,9 @@ public class Bullet : MonoBehaviour
         if (!NeedMove)
             return;
 
-        Vector3 moveVector = MoveDirection.normalized * Speed * Time.deltaTime;
 
+        Vector3 moveVector = MoveDirection.normalized * Speed * Time.deltaTime;
+        moveVector = AdjustMove(moveVector);
         transform.position += moveVector;
     }
 
@@ -50,5 +59,68 @@ public class Bullet : MonoBehaviour
         Speed = speed;
 
         NeedMove = true;
+        FiredTime = Time.time;
+    }
+
+    Vector3 AdjustMove(Vector3 moveVector)
+    {
+        RaycastHit hitInfo;
+
+        if (Physics.Linecast(transform.position, transform.position + moveVector, out hitInfo))
+        {
+            moveVector = hitInfo.point - transform.position;
+            OnBulletCollision(hitInfo.collider);
+        }
+        return moveVector;
+    }
+
+    void OnBulletCollision(Collider collider)
+    {
+        if (Hited)
+            return;
+        // 콜리더 부하 방지
+        Collider myCollider = GetComponentInChildren<Collider>();
+        myCollider.enabled = false;
+
+        Hited = true;
+        NeedMove = false;
+
+        Debug.Log("OnBulletCollision cllider = " + collider.name);
+
+        if(ownerSide == OwnerSide.Player)
+        {
+            Enemy enemy = collider.GetComponentInParent<Enemy>();
+        }
+        else
+        {
+            Player player = collider.GetComponentInParent<Player>();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OnBulletCollision(other);
+    }
+
+    bool ProcessDisappearCondition()
+    {
+        if(transform.position.x > 15.0f || transform.position.x < -15.0f
+            || transform.position.y > 15.0f || transform.position.y < -15.0f)
+        {
+            Disappear();
+            return true;
+        }
+        else if (Time.time - FiredTime > LifeTime)
+        {
+            Disappear();
+            return true;
+        }
+
+        return false;
+    }
+
+    void Disappear()
+    {
+        Destroy(gameObject);
     }
 }
