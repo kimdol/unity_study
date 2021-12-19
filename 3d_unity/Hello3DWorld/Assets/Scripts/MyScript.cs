d67a973348a61c6fb5c2ec1fade15d8c17856ecb
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2021-12-19 15:06:15 +0900

    sin

--- 3d_unity/Hello3DWorld/Assets/Scripts/MyScript.cs
+++ 3d_unity/Hello3DWorld/Assets/Scripts/MyScript.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+using System.Collections;   // 컨테이너
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,12 +7,15 @@ public class MyScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hellow World!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(Time.time);
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Sin(Time.time * 5.0f);
+        transform.position = pos;
     }
 }
