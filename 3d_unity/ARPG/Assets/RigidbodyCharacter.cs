91e5e98480ec099bd1b7299bd93875145ff4437a
rlagkd2451@gmail.com <rlagkd2451@gmail.com>
2022-03-19 21:07:49 +0900

    qwer

--- 3d_unity/ARPG/Assets/RigidbodyCharacter.cs
+++ 3d_unity/ARPG/Assets/RigidbodyCharacter.cs
@@ -4,17 +4,6 @@ using UnityEngine;
 
 public class RigidbodyCharacter : MonoBehaviour
 {
-    #region Variables
-    public float speed = 5.0f;
-    public float jumpHeight = 2.0f;
-    public float dashDistance = 5.0f;
-
-    private Rigidbody rigidbody;
-
-    private Vector3 inputDirection = Vector3.zero;
-
-    #endregion Variables
-
     // Start is called before the first frame update
     void Start()
     {
