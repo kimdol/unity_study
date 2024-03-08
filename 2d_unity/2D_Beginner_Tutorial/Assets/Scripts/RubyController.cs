5c90950aacd6c8baa09911a837da68b84cccf0d2
kimdol <86827391+kimdol@users.noreply.github.com>
2024-03-08 11:38:52 +0900

    Fix RubyController

--- 2d_unity/2D_Beginner_Tutorial/Assets/Scripts/RubyController.cs
+++ 2d_unity/2D_Beginner_Tutorial/Assets/Scripts/RubyController.cs
@@ -5,6 +5,9 @@ using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class RubyController : MonoBehaviour
 {
+    public float speed;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +18,10 @@ public class RubyController : MonoBehaviour
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f * horizontal;
+        position.x = position.x + speed * Time.deltaTime * horizontal;
+        position.y = position.y + speed * Time.deltaTime * vertical;
         transform.position = position;
 
     }
