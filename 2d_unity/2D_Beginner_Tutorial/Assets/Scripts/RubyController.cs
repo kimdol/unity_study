04f59b391ca2030922db9f788feaba1ff0bb9779
kimdol <86827391+kimdol@users.noreply.github.com>
2024-06-24 10:31:53 +0900

    Add ruby animator

--- 2d_unity/2D_Beginner_Tutorial/Assets/Scripts/RubyController.cs
+++ 2d_unity/2D_Beginner_Tutorial/Assets/Scripts/RubyController.cs
@@ -19,11 +19,15 @@ public class RubyController : MonoBehaviour
     bool isInvincible;
     float invincibleTimer;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
 
         currentHealth = maxHealth;
 
@@ -36,11 +40,24 @@ public class RubyController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        Vector2 position = rigidbody2d.position;
-        position.x = position.x + speed * Time.deltaTime * horizontal;
-        position.y = position.y + speed * Time.deltaTime * vertical;
+        Vector2 move = new Vector2(horizontal, vertical);
 
-        
+
+        if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+
+        animator.SetFloat("Look X", lookDirection.x); 
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
+
+
+        Vector2 position = rigidbody2d.position;
+        position = position + move * speed * Time.deltaTime;
+
+
         rigidbody2d.MovePosition(position);
 
 
